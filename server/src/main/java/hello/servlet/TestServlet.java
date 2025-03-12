eb596c531d81f24a4262a0b83b0054d6ce7f44f5
이승환 <seaung13@gmail.com>
2025-03-12 17:13:57 +0900

    feat: [server] 웹 서버와 서블릿 컨테이너 - 예제 추가 후 WAR 빌드 및 배포

--- server/src/main/java/hello/servlet/TestServlet.java
+++ server/src/main/java/hello/servlet/TestServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 /**
  * http://localhost:8080/test
  */
-@WebServlet(urlPatterns="/test")
+@WebServlet(urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
 
     @Override
