ebc0e7974f8d3c1030da99d2a08d147c0e8c2382
이승환 <seaung13@gmail.com>
2025-03-12 12:37:40 +0900

    chore: [server] 웹 서버와 서블릿 컨테이너 - 프로젝트 추가

---
+++ server/src/test/java/hello/servlet/TestServlet.java
@@ -0,0 +1,22 @@
+package hello.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * http://localhost:8080/test
+ */
+@WebServlet(urlPatterns="/test")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("TestServlet.service");
+        response.getWriter().println("test");
+    }
+}
