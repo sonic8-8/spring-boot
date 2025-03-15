b68e717adbb12cddae66c33d29b524cc29bde9a9
이승환 <seaung13@gmail.com>
2025-03-15 16:28:13 +0900

    test: [external] 외부설정과 프로필1 - 커맨드 라인 옵션 인수 학습 테스트 추가

--- external/src/test/java/hello/external/CommandLineV1.java
+++ external/src/test/java/hello/external/CommandLineV1.java
@@ -1,6 +1,10 @@
 package hello.external;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.DefaultApplicationArguments;
+
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 public class CommandLineV1 {
@@ -9,5 +13,24 @@ public class CommandLineV1 {
         for (String arg : args) {
             log.info("arg {}", arg);
         }
+
+        DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+        log.info("SourceArgs = {}", List.of(appArgs.getSourceArgs()));
+        log.info("NonOptionArgs = {}", appArgs.getNonOptionArgs());
+        log.info("OptionNames = {}", appArgs.getOptionNames());
+
+        Set<String> optionNames = appArgs.getOptionNames();
+        for (String optionName : optionNames) {
+            log.info("option args {}={}", optionName, appArgs.getOptionValues(optionName));
+        }
+
+        List<String> url = appArgs.getOptionValues("url");
+        List<String> username = appArgs.getOptionValues("username");
+        List<String> password = appArgs.getOptionValues("password");
+        List<String> mode = appArgs.getOptionValues("mode");
+        log.info("url={}", url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+        log.info("mode={}", mode);
     }
 }
