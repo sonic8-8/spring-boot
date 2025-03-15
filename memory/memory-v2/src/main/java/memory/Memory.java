3bbd6b406e0d173caeddb0066db49ad8d4c820be
이승환 <seaung13@gmail.com>
2025-03-15 15:18:59 +0900

    feat: [memory-v2] 자동 구성 - 자동 구성 라이브러리 제작

---
+++ memory/memory-v2/src/main/java/memory/Memory.java
@@ -0,0 +1,13 @@
+package memory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Memory {
+    private long used;
+    private long max;
+}
