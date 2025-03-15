c91829c5a4246264e5184a7bc26f0bb549287a0b
이승환 <seaung13@gmail.com>
2025-03-15 12:20:55 +0900

    feat: [memory-v1] 자동 구성 - 순수 라이브러리 제작

---
+++ memory-v1/src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,18 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+
+}
